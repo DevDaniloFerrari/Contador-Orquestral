a1b854bc4aa32137a3d21e314e33b080f9965202
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2020-01-04 02:31:28 -0300

    Padronizando o componente Instrumento

--- src/app/components/instrumento/instrumento.component.ts
+++ src/app/components/instrumento/instrumento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-instrumento',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./instrumento.component.scss'],
 })
 export class InstrumentoComponent implements OnInit {
+
+  @Input() public nomeDoInstrumento: string;
+  @Input() public caminhoDaImagem: string;
 
   constructor() { }
 
